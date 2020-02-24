52faaf899acbf13a69751f4261337f1372fb52fb
Emad Hosseini <EmadHosseini@hotmail.com>
2020-02-24 21:58:30 +0330

    First module controller hit

---
+++ src/BasicModules/SigmaSharp.Stern.Module.UserStorage/UserStorageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SigmaSharp.Stern.Module.UserStorage
+{
+    [Route("/")]
+    public class UserStorageController : Controller
+    {
+        [HttpGet]
+        public async Task<string> GetValue()
+        {
+            return "Hello";
+        }
+    }
+}
