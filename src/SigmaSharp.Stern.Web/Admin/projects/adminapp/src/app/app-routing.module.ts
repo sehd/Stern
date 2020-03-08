1e17de16080e4c4353258bcb4aa4922d3c0c730c
Emad Hosseini <EmadHosseini@hotmail.com>
2020-03-08 19:54:18 +0330

    Routing and component structure fixed in admin

--- src/SigmaSharp.Stern.Web/Admin/projects/adminapp/src/app/app-routing.module.ts
+++ src/SigmaSharp.Stern.Web/Admin/projects/adminapp/src/app/app-routing.module.ts
@@ -1,11 +1,53 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavComponent } from './home/nav/nav.component';
+import { ChangePassComponent } from './home/nav/change-pass/change-pass.component';
+import { HeaderComponent } from './home/header/header.component';
+
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { ModuleSettingsComponent } from './home/module-settings/module-settings.component';
+import { ModuleDetailsComponent } from './home/module-details/module-details.component';
+import { UsersComponent } from './home/users/users.component';
+import { AdminsComponent } from './home/admins/admins.component';
+import { AppSettingsComponent } from './home/app-settings/app-settings.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'modules', component: ModuleSettingsComponent },
+      { path: 'modules/:name', component: ModuleDetailsComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'admins', component: AdminsComponent },
+      { path: 'settings', component: AppSettingsComponent },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RouteComponents = [
+  NavComponent,
+  HeaderComponent,
+  DashboardComponent,
+  ModuleSettingsComponent,
+  ModuleDetailsComponent,
+  UsersComponent,
+  AdminsComponent,
+  AppSettingsComponent,
+  ChangePassComponent,
+  LoginComponent,
+  HomeComponent,
+  NotFoundComponent
+]
--- src/SigmaSharp.Stern.Web/Admin/projects/adminapp/src/app/app.module.ts
+++ src/SigmaSharp.Stern.Web/Admin/projects/adminapp/src/app/app.module.ts
@@ -1,31 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RouteComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_BASE_HREF } from '@angular/common';
-import { NavComponent } from './nav/nav.component';
-import { HeaderComponent } from './header/header.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ModuleSettingsComponent } from './module-settings/module-settings.component';
-import { ModuleDetailsComponent } from './module-details/module-details.component';
-import { UsersComponent } from './users/users.component';
-import { AdminsComponent } from './admins/admins.component';
-import { AppSettingsComponent } from './app-settings/app-settings.component';
-import { ChangePassComponent } from './nav/change-pass/change-pass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    HeaderComponent,
-    DashboardComponent,
-    ModuleSettingsComponent,
-    ModuleDetailsComponent,
-    UsersComponent,
-    AdminsComponent,
-    AppSettingsComponent,
-    ChangePassComponent
+    RouteComponents
   ],
   imports: [
     BrowserModule,
