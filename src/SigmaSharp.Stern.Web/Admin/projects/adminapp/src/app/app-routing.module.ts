c6c70fc403d31fa2df39386fa3b63b837d0b82e5
Emad Hosseini <EmadHosseini@hotmail.com>
2020-03-09 12:15:00 +0330

    Added auth service to admin

--- src/SigmaSharp.Stern.Web/Admin/projects/adminapp/src/app/app-routing.module.ts
+++ src/SigmaSharp.Stern.Web/Admin/projects/adminapp/src/app/app-routing.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { NavComponent } from './home/nav/nav.component';
-import { ChangePassComponent } from './home/nav/change-pass/change-pass.component';
-import { HeaderComponent } from './home/header/header.component';
+import { AuthGuard } from './services/auth/auth.guard';
 
-import { DashboardComponent } from './home/dashboard/dashboard.component';
-import { ModuleSettingsComponent } from './home/module-settings/module-settings.component';
-import { ModuleDetailsComponent } from './home/module-details/module-details.component';
-import { UsersComponent } from './home/users/users.component';
-import { AdminsComponent } from './home/admins/admins.component';
-import { AppSettingsComponent } from './home/app-settings/app-settings.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NavComponent } from './components/home/nav/nav.component';
+import { ChangePassComponent } from './components/home/nav/change-pass/change-pass.component';
+import { HeaderComponent } from './components/home/header/header.component';
+
+import { DashboardComponent } from './components/home/dashboard/dashboard.component';
+import { ModuleSettingsComponent } from './components/home/module-settings/module-settings.component';
+import { ModuleDetailsComponent } from './components/home/module-details/module-details.component';
+import { UsersComponent } from './components/home/users/users.component';
+import { AdminsComponent } from './components/home/admins/admins.component';
+import { AppSettingsComponent } from './components/home/app-settings/app-settings.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: DashboardComponent },
       { path: 'modules', component: ModuleSettingsComponent },
--- src/SigmaSharp.Stern.Web/Admin/projects/adminapp/src/app/app.module.ts
+++ src/SigmaSharp.Stern.Web/Admin/projects/adminapp/src/app/app.module.ts
@@ -2,8 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, RouteComponents } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './services/auth/jwt.interceptor';
+import { ErrorInterceptor } from './services/auth/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,11 @@ import { APP_BASE_HREF } from '@angular/common';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/admin' }],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/admin' },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
